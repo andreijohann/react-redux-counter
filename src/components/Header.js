58586bb68cda62beb8eed6e166f987b969473fcd
Andrei Johann <andrei_johann@yahoo.com>
2022-03-15 13:39:08 +1000

    Spliting Slices code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import classes from "./Header.module.css";
 import { useSelector, useDispatch } from "react-redux";
-import { authActions } from "../store/store";
+import { authActions } from "../store/authSlice";
 
 const Header = () => {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
